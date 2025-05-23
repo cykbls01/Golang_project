7089162934223d55fe330f235699cfaa8ec5b491
cykbls01 <2541601705@qq.com>
2025-05-23 15:15:53 +0800

    add opa

--- basic/opa.go
+++ basic/opa.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"basic/util"
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/kr/pretty"
 	_ "github.com/kr/pretty"
+	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"reflect"
+	"strings"
 )
 
 type Data struct {
@@ -18,6 +25,64 @@ type Data struct {
 	PodName     string   `json:"pod_name"`
 	Privileged  bool     `json:"privileged"`
 	HostPaths   []string `json:"host_paths"`
+}
+
+type Namespace struct {
+	ClusterName string `json:"cluster_name"`
+	ConfigPath  string `json:"config_path"`
+	Namespace   string `json:"namespace"`
+}
+
+func ExecuteComplexCmd(cmdStr string) (string, string, error) {
+	// 获取当前工作目录[4](@ref)
+	//fmt.Println(cmdStr)
+	currentDir, err := os.Getwd()
+	if err != nil {
+		return "", "", fmt.Errorf("获取目录失败: %v", err)
+	}
+
+	// 创建带bash解析的命令对象[1,6](@ref)
+	cmd := exec.Command("/bin/bash", "-c", cmdStr)
+	cmd.Dir = currentDir // 显式设置工作目录
+
+	// 分离标准输出和错误缓冲区[3](@ref)
+	var stdoutBuf, stderrBuf bytes.Buffer
+	cmd.Stdout = &stdoutBuf
+	cmd.Stderr = &stderrBuf
+
+	// 执行并等待命令完成[6](@ref)
+	err = cmd.Run()
+	stdout := stdoutBuf.String()
+	stderr := stderrBuf.String()
+
+	return stdout, stderr, err
+}
+
+func ListAllImages(pods []v1.Pod) []string {
+	// 使用 map 实现去重
+	imageSet := make(map[string]struct{})
+	var uniqueImages []string
+
+	// 遍历所有 Pod 的容器
+	for _, pod := range pods {
+		// 获取普通容器
+		for _, container := range pod.Spec.Containers {
+			if _, exists := imageSet[container.Image]; !exists {
+				imageSet[container.Image] = struct{}{}
+				uniqueImages = append(uniqueImages, container.Image)
+			}
+		}
+
+		// 获取初始化容器[2](@ref)
+		for _, initContainer := range pod.Spec.InitContainers {
+			if _, exists := imageSet[initContainer.Image]; !exists {
+				imageSet[initContainer.Image] = struct{}{}
+				uniqueImages = append(uniqueImages, initContainer.Image)
+			}
+		}
+	}
+
+	return uniqueImages
 }
 
 func ListAllFiles(dirPath string) ([]string, error) {
@@ -39,63 +104,135 @@ func ListAllFiles(dirPath string) ([]string, error) {
 	return files, err
 }
 
-var result map[string]Data
-
-func main() {
-	files, _ := ListAllFiles(os.Args[1])
-	result = make(map[string]Data)
-	for _, file := range files {
-		Run(file)
-	}
-	pretty.Println(result)
-}
-
-func Run(kubeconfigPath string) {
-
-	excludeNamespaces := []string{"kube-system", "arms-prom", "falco"} // 默认排除的系统命名空间
-
-	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+func ListAllPods(namespace Namespace) []v1.Pod {
+	config, err := clientcmd.BuildConfigFromFlags("", namespace.ConfigPath)
 	if err != nil {
-		panic(err.Error())
+		fmt.Println(err.Error())
+		return []v1.Pod{}
 	}
 
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
-		panic(err.Error())
+		fmt.Println(err.Error())
+		return []v1.Pod{}
+	}
+
+	pods, _ := clientset.CoreV1().Pods(namespace.Namespace).List(context.TODO(), metav1.ListOptions{})
+	return pods.Items
+}
+func ListAllNamespaces(kubeconfigPath string) []Namespace {
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfigPath)
+	if err != nil {
+		fmt.Println(err.Error())
+		return []Namespace{}
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Println(err.Error())
+		return []Namespace{}
 	}
 
 	// 获取所有命名空间
 	namespaces, err := clientset.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
-		panic(err.Error())
+		fmt.Println(err.Error())
+		return []Namespace{}
 	}
 
-	// 遍历所有命名空间
-	for _, ns := range namespaces.Items {
-		nsName := ns.Name
+	var res = make([]Namespace, 0)
+	for _, namespace := range namespaces.Items {
+		if isExcluded(namespace.Name) {
+			continue
+		}
+		res = append(res, Namespace{
+			ClusterName: filepath.Base(kubeconfigPath),
+			ConfigPath:  kubeconfigPath,
+			Namespace:   namespace.Name,
+		})
+	}
+	return res
+}
+
+func WriteSliceToFile(data interface{}, filename string) error {
+	// 类型校验：必须为切片类型
+	if reflect.ValueOf(data).Kind() != reflect.Slice {
+		return os.ErrInvalid
+	}
+
+	// 序列化为带缩进的JSON格式
+	formattedData, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return err
+	}
+
+	// 原子化写入文件（覆盖模式+自动创建）
+	return os.WriteFile(filename, formattedData, 0644)
+}
+
+func isExcluded(ns string) bool {
+	excludeList := []string{"kube-system", "arms-prom", "falco", "kube-node-lease", "kube-public"}
+	for _, excluded := range excludeList {
+		if excluded == ns {
+			return true
+		}
+	}
+	return false
+}
+
+func image_check(file string) {
+	for _, ns := range ListAllNamespaces(file) {
+		os.Mkdir("image_check/"+filepath.Base(file), 0755)
+		os.Mkdir("image_check/"+filepath.Base(file)+"/"+ns.Namespace, 0755)
+		for _, image := range ListAllImages(ListAllPods(ns)) {
+			cmd1 := "trivy image --skip-db-update --skip-java-db-update  " + image + " --severity=CRITICAL  --report=summary --insecure --format template --template \"@/data/.cache/trivy/html.tpl\" --output image_check/" + filepath.Base(file) + "/" + ns.Namespace + "/" + filepath.Base(image) + ".html --timeout 3m -q --cache-dir /data/.cache/trivy"
+			cmd2 := "trivy image --skip-db-update --skip-java-db-update  " + image + " --severity=CRITICAL  --report=summary --insecure --format json --output image_check/" + filepath.Base(file) + "/" + ns.Namespace + "/" + filepath.Base(image) + ".json --timeout 3m -q --cache-dir /data/.cache/trivy"
+			switch {
+			case strings.HasPrefix(image, "cr-ee"):
+				ExecuteComplexCmd(cmd1)
+				ExecuteComplexCmd(cmd2)
+			case strings.HasPrefix(image, "swr.bj-kjy-50"):
+				ExecuteComplexCmd(cmd1 + " --username " + util.Config.MP["swr5-username"] + " --password " + util.Config.MP["swr-password"])
+				ExecuteComplexCmd(cmd2 + " --username " + util.Config.MP["swr5-username"] + " --password " + util.Config.MP["swr-password"])
+			case strings.HasPrefix(image, "swr.bj-kjy-90"):
+				ExecuteComplexCmd(cmd1 + " --username " + util.Config.MP["swr9-username"] + " --password " + util.Config.MP["swr-password"])
+				ExecuteComplexCmd(cmd2 + " --username " + util.Config.MP["swr9-username"] + " --password " + util.Config.MP["swr-password"])
+			case strings.HasPrefix(image, "swr.sh-ky-70"):
+				ExecuteComplexCmd(cmd1 + " --username " + util.Config.MP["swr7-username"] + " --password " + util.Config.MP["swr-password"])
+				ExecuteComplexCmd(cmd2 + " --username " + util.Config.MP["swr7-username"] + " --password " + util.Config.MP["swr-password"])
+			default:
+				fmt.Println("未知前缀: " + image)
+			}
+
+		}
+	}
+
+}
+
+func pod_check(path string) {
+	var result = make(map[string]Data)
+	namespaces := ListAllNamespaces(path)
+	for _, ns := range namespaces {
+		result = make(map[string]Data)
+		nsName := ns.Namespace
 		// 检查是否在排除列表中
-		if isExcluded(nsName, excludeNamespaces) {
+		if isExcluded(nsName) {
 			continue
 		}
 
 		// 获取命名空间中的所有Pod
-		pods, err := clientset.CoreV1().Pods(nsName).List(context.TODO(), metav1.ListOptions{})
-		if err != nil {
-			fmt.Printf("Error getting pods in namespace %s: %v\n", nsName, err)
-			continue
-		}
+		pods := ListAllPods(ns)
 
 		// 遍历Pod中的容器
-		for _, pod := range pods.Items {
+		for _, pod := range pods {
 			podName := pod.Name
-			//foundIssues := false
 
 			// 检查容器安全上下文
 			for _, container := range pod.Spec.Containers {
 				// 检查特权模式
 				if container.SecurityContext != nil && container.SecurityContext.Privileged != nil && *container.SecurityContext.Privileged {
 					result[podName] = Data{
-						ClusterName: filepath.Base(kubeconfigPath),
+						ClusterName: filepath.Base(path),
 						Namespace:   nsName,
 						PodName:     podName,
 						Privileged:  true,
@@ -115,7 +252,7 @@ func Run(kubeconfigPath string) {
 						value.HostPaths = append(value.HostPaths, volume.HostPath.Path)
 					} else {
 						result[podName] = Data{
-							ClusterName: filepath.Base(kubeconfigPath),
+							ClusterName: filepath.Base(path),
 							Namespace:   nsName,
 							PodName:     podName,
 							Privileged:  false,
@@ -125,15 +262,27 @@ func Run(kubeconfigPath string) {
 				}
 			}
 		}
+
+		res := make([]Data, 0)
+		for _, value := range result {
+			res = append(res, value)
+		}
+		if len(res) == 0 {
+			continue
+		}
+		os.Mkdir("pod_check/"+filepath.Base(path), 0755)
+		WriteSliceToFile(res, "pod_check/"+filepath.Base(path)+"/"+ns.Namespace+".res")
 	}
 }
 
-// isExcluded 检查命名空间是否在排除列表中
-func isExcluded(ns string, excludeList []string) bool {
-	for _, excluded := range excludeList {
-		if excluded == ns {
-			return true
-		}
+func main() {
+	util.Init()
+	files, _ := ListAllFiles(util.Config.MP["path"])
+	pretty.Println(files)
+	os.Mkdir("pod_check", 0755)
+	os.Mkdir("image_check", 0755)
+	for _, file := range files {
+		image_check(file)
+		pod_check(file)
 	}
-	return false
 }
